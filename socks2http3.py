2094a8be7d47d16144f5b26a056f9427416471a3
bloodmage <zhouyisu@gmail.com>
2019-08-05 04:56:39 +0800

    Update socks2http3.py
    Fixed encode/decode
    Binary data no longer suffer decode problem
    Pump data with correct timeout calculating process

--- socks2http3.py
+++ socks2http3.py
@@ -9,91 +9,104 @@ VERSION = 'socks2http/0.01'
 HTTPVER = 'HTTP/1.1'
 
 async def get_request(reader, writer):
-    req = ''
+    req = b''
     while True:
         data = await reader.read(8192)
         if len(data) <= 0:
             return None, None, None, None
-        req += data.decode()
-        end = req.find('\n')
+        req += data
+        end = req.find(b'\n')
         if end != -1:
             break
     addr = writer.get_extra_info('peername')
-    print('%r: %s'%(req[:end], addr))
-    return req[:end+1].split() + [req[end+1:],]
+    print('%r: %s'%(req[:end].decode('utf-8', 'replace'), addr))
+    return req[:end+1].split(b' ') + [req[end+1:],]
 
 async def connect_target(host):
+    host = host.decode('utf-8')
     i = host.find(':')
     if i!=-1:
         port = int(host[i+1:])
         host = host[:i]
     else:
         port = 80
-    (soc_family, _, _, _, address) = socket.getaddrinfo(host, port)[0]
+    #(soc_family, _, _, _, address) = socket.getaddrinfo(host, port)[0]
     target = socks3.socksocket()
-    target.setproxy(socks3.PROXY_TYPE_SOCKS5, '127.0.0.1', 8087)
+    target.setproxy(socks3.PROXY_TYPE_SOCKS5, '127.0.0.1', 12948)
     try:
-        await target.connect(address)
-    except:
+        await target.connect((host,port))
+    except Exception as e:
         target.close()
         target = None
+        import traceback
+        traceback.print_exc()
+    #print("target connected")
     return target
 
-async def handle_socks(socks_reader, http_writer, stats):
+async def pump(reader, writer, stats, mychan, killevent, timeout = 30, bulk = 8192):
+    killwait = loop.create_task(killevent.wait())
+    readwait = loop.create_task(reader.read(bulk))
     while True:
-        data = await socks_reader.read(8192)
-        if len(data) <= 0:
-            break
-        stats["down"] += len(data)
-        http_writer.write(data)
-        await http_writer.drain()
-    http_writer.close()
+        if readwait is None: readwait = loop.create_task(reader.read(bulk))
+        done, pending = await asyncio.wait([killwait, readwait], loop = loop, timeout=timeout, return_when=asyncio.FIRST_COMPLETED)
+        if readwait in done:
+            data = await readwait
+            if len(data) <= 0:
+                break
+            stats[mychan+'timeout'] = False
+            stats[mychan] += len(data)
+            writer.write(data)
+            readwait = None
+        if killwait in done:
+            break #To kill
+        if len(done)==0:
+            #A timeout
+            stats[mychan+'timeout'] = True
+            #If all timeout, stop
+            if stats['uptimeout'] and stats['downtimeout']:
+                break
+    #To kill
+    killevent.set()
+    writer.close()
 
 async def handle_http(reader, writer):
-    stats = {"up":0, "down":0}
+    stats = {"up":0, "down":0, "uptimeout":False, "downtimeout":False}
     method, path, protocol, data = await get_request(reader, writer) 
     if method == None:
-        writer.close()
         return
-    elif method == 'CONNECT':
+    elif method == b'CONNECT':
         target = await connect_target(path)
         if not target:
-            writer.close()
             return
         writer.write((HTTPVER+' 200 Connection established\n'+
                                          'Proxy-agent: %s\n\n'%VERSION).encode())
-        await writer.drain()
-    elif method in ('OPTIONS', 'GET', 'HEAD', 'POST', 'PUT',
-                                 'DELETE', 'TRACE'):
+    elif method in (b'OPTIONS', b'GET', b'HEAD', b'POST', b'PUT',
+                                 b'DELETE', b'TRACE'):
         url = path[7:]
-        i = url.find('/')
+        i = url.find(b'/')
         host = url[:i]
         url = url[i:]
         target = await connect_target(host)
         if not target:
-            writer.close()
+            print("Not target", target)
             return
-        target.send(('%s %s %s\n'%(method, url, protocol)+ data).encode())
+        target.send(b'%s %s %s'%(method, url, protocol)+ data)
     else:
-        print("HTTPProxy protocol error")
+        print("HTTPProxy protocol error", method)
+        return
 
+    killpipeevent = asyncio.Event(loop = loop)
     socks_reader, socks_writer = await asyncio.open_connection(sock=target, loop=loop)
-    asyncio.ensure_future(handle_socks(socks_reader, writer, stats))
-    while True:
-        data = await reader.read(512)
-        if len(data) <= 0:
-            break
-        stats["up"] += len(data)
-        socks_writer.write(data)
-        await socks_writer.drain()
+    downpump = pump(socks_reader, writer, stats, 'down', killpipeevent)
+    uppump = pump(reader, socks_writer, stats, 'up', killpipeevent)
+    await asyncio.wait([uppump, downpump], loop = loop)
 
     print("FINISH", path, stats)
-    writer.close()
     socks_writer.close()
 
 logging.basicConfig(level=logging.DEBUG)
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_http, '127.0.0.1', 8080, loop=loop)
+coro = asyncio.start_server(handle_http, '127.0.0.1', 12949, loop=loop)
 server = loop.run_until_complete(coro)
 
 # Serve requests until Ctrl+C is pressed
